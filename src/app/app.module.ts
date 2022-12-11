ca49a95bca82e780e43d658d8c719092c2f10f6f
Bharat <bharat.mane@gmail.com>
2022-12-11 20:01:42 +0530

    feat: add new components for links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {  Router, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FavLinksComponent } from './fav-links/fav-links.component';
+
+const appRoutes: Routes = [
+  { path: "links", component: FavLinksComponent },
+  { path: "", component: AppComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule],
+    bootstrap: [AppComponent]
+    
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router){
+
+  }
+ }
